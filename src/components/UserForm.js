88d9880d074c9f5d3c520c86cd0560f41faf5e86
New <chauhanraunaq878@gmail.com>
2019-10-05 17:55:08 +0530

    user details component

--- src/components/UserForm.js
+++ src/components/UserForm.js
@@ -1,4 +1,5 @@
 import React, { Component} from 'react';
+import FormUserDetails from './FormUserDetails';
 
 class UserForm extends Component {
 
@@ -28,13 +29,34 @@ class UserForm extends Component {
         });
     }
 
-    render() {
-        return (
-            <div>
+    //Handle fields change
+    handleChange = input => e => {
+        this.setState({[input]: e.target.value});
+    }
 
-            </div>
-        )
+    render() {
+        const { step } = this.state;
+        const { firstName, lastName, email, occupation, city, bio} = this.state;
+        const values = { firstName, lastName, email, occupation, city, bio};
+
+        switch(step) {
+            case 1: 
+                return (
+                    <FormUserDetails
+                        nextStep={this.nextStep}
+                        handleChange={this.handleChange}
+                        values={values} 
+                    />
+                )
+            case 2: 
+                return <h1>FormPersonalDetails</h1>
+            case 3:
+                return <h1>Confirm</h1>
+            case 4:
+                return <h1>Success</h1>
+            default:
+        }
     }
 }
 
-export default UserForm
+export default UserForm;
